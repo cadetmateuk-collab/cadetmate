4d2f8efc250b5b29a3bd6a1f6e658c66bba5c978
cadetmate <cadetmateuk@gmail.com>
2026-02-01 13:39:09 +0000

    Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { CadetMateSidebar } from "../components/Sidebar";
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen w-full">
-      <CadetMateSidebar />
-      <main className="flex-1 p-8">
-        <h1 className="text-3xl font-bold">Welcome</h1>
-        {/* Your page content here */}
-        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-<stripe-pricing-table pricing-table-id="prctbl_1SkSReRwygITQzeHqWUlfbRI"
-publishable-key="pk_test_51S8R2vRwygITQzeHn6B8EW7O3AmdwJHQBknayUD9sO2o7byW50Cp3uuxFL4VW9HDykuCjtdV0D2xoWj3jk8wZFAo0025ArN1iY">
-</stripe-pricing-table>
-      </main>
-    </div>
-  );
+export default function RootPage() {
+  redirect('/home')
 }
