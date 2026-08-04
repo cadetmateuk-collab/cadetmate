c27a32aad7d5bb26c92533ae0a209824693cb222
cadetmate <editsbyadz@gmail.com>
2026-08-04 23:22:51 +0100

    Refactor TypeScript definitions for 'stripe-pricing-table' component and update tsconfig.server.json to streamline file inclusion and exclusion. This enhances type safety and improves project structure.

--- stripe.d.ts
+++ stripe.d.ts
@@ -1,8 +1,17 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'stripe-pricing-table': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      'pricing-table-id'?: string;
-      'publishable-key'?: string;
-    };
+import type * as React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'stripe-pricing-table': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        'pricing-table-id'?: string;
+        'publishable-key'?: string;
+      };
+    }
   }
 }
+
+export {};
